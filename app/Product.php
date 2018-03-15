05796973847b833a394f7d87a07702fa344c63e9
Tangeni <tangenishikomba@gmail.com>
2018-03-15 23:33:18 +0200

    product model updated

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    const AVAILABLE_PRODUCT = 'available';
+    const UNAVAILABLE_PRODUCT = 'unavailable';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id'
+    ];
+
+    /**
+     * @return bool
+     *
+     * checks if product is available
+     */
+    public function isAvailable(){
+        return $this->status == Product::AVAILABLE_PRODUCT;
+    }
+
+    public function seller(){
+        return $this->belongsTo(Seller::class);
+    }
+
+    public function transactions(){
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function categories(){
+        $this->belongsToMany(Category::class);
+    }
+
+
 }
