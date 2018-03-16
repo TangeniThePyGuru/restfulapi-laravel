86db2a699deb4b6a3bc42ff8e429498cfa01bc81
Tangeni <tangenishikomba@gmail.com>
2018-03-16 16:30:25 +0200

    missing return keyword added

--- app/Product.php
+++ app/Product.php
@@ -36,7 +36,7 @@ class Product extends Model
     }
 
     public function categories(){
-        $this->belongsToMany(Category::class);
+       return $this->belongsToMany(Category::class);
     }
 
 
