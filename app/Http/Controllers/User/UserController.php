0e98491afc0188a8501333dfacfe8e49ff78cb7f
Tangeni <tangenishikomba@gmail.com>
2018-03-19 00:10:38 +0200

    implicit model binding updated on the User Controller

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -54,12 +54,11 @@ class UserController extends ApiController
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  User $user
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
-        $user = User::findOrfail($id);
 
         return $this->showOne($user);
     }
@@ -69,12 +68,11 @@ class UserController extends ApiController
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  User $user
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user)
     {
-        $user = User::findOrfail($id);
 
         $rules = [
             'email' => 'email|unique:users,email,' . $user->id ,
@@ -125,12 +123,11 @@ class UserController extends ApiController
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  User $user
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        $user = User::findOrFail($id);
 
         $user->delete();
         // code 200 means successful
