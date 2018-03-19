e97816d2e87a106d540d38d2f9a79a9fa91e4111
Tangeni <tangenishikomba@gmail.com>
2018-03-19 22:33:31 +0200

    BuyerProductController implemented

---
+++ app/Http/Controllers/Buyer/BuyerProductController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Buyer;
+
+use App\Buyer;
+use App\Http\Controllers\ApiController;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class BuyerProductController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     * @param Buyer $buyer
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Buyer $buyer)
+    {
+        // eager loading at work
+        // pluck enables you to ignore the other part of the object, and only return the
+        // specified attribute
+        $products = $buyer->transactions()->with('product')
+            ->get()
+            ->pluck('product');
+
+        return $this->showAll($products);
+    }
+
+}
