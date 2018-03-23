7f793e809c39d99e1e3470e8b2ec5b8e1c6401cf
Tangeni <tangenishikomba@gmail.com>
2018-03-24 00:13:11 +0200

    Product Category Controller destroy method implemented

--- app/Http/Controllers/Product/ProductCategoryController.php
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -37,4 +37,17 @@ class ProductCategoryController extends ApiController
         return $this->showAll($product->categories);
     }
 
+
+    public function destroy(Product $product, Category $category){
+
+        if (!$product->categories()->find($category->id)){
+            return $this->errorResponse('The specified category is not a category of this product', 404);
+        }
+
+        $product->categories()->detach($category->id);
+
+        return $this->showAll($product->categories);
+
+    }
+
 }
