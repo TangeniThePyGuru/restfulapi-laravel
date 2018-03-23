d55e12e6046a34d5d21181cbfd4789361d641b70
Tangeni <tangenishikomba@gmail.com>
2018-03-23 20:06:42 +0200

    Seller Product Controller update and delete method implemented

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -7,6 +7,7 @@ use App\Product;
 use App\Seller;
 use App\User;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class SellerProductController extends ApiController
 {
@@ -43,7 +44,7 @@ class SellerProductController extends ApiController
         $rules = [
           'name' => 'required',
           'description' => 'required',
-          'quantity' => 'required|integer|min:',
+          'quantity' => 'required|integer|min:1',
           'image' => 'required|image'
         ];
 
@@ -87,22 +88,70 @@ class SellerProductController extends ApiController
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Seller  $seller
+     * @param Product $product
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Seller $seller)
+    public function update(Request $request, Seller $seller, Product $product)
     {
-        //
+        $rules = [
+            'quantity' => 'integer|min:1',
+            'status' => 'in:'.Product::AVAILABLE_PRODUCT .','.Product::UNAVAILABLE_PRODUCT,
+            'image' => 'image'
+        ];
+
+        $this->validate($request, $rules);
+
+        // checks if the the provided seller is owner of this products
+        $this->checkSeller($seller, $product);
+
+        // if checkSeller is successful
+        $product->fill($request->only([
+            'name',
+            'description',
+            'quantity'
+        ]));
+
+        if ($request->has('status')){
+            $product->status = $request->status;
+            // if a product is available that it should also belong to categories
+            if ($product->isAvailable() && $product->categories()->count() == 0){
+                // code 409 == conflict
+                return $this->errorResponse('An active product must have at least one category', 409);
+            }
+        }
+        // represents that nothing has changed
+        if ($product->isClean()){
+            return $this->errorResponse('You need to specify a different value to update', 422);
+        }
+
+        // if something changed update the product
+        $product->save();
+
+        return $this->showOne($product);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Seller  $seller
+     * @param Product $product
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Seller $seller)
+    public function destroy(Seller $seller, Product $product)
     {
-        //
+        $this->checkSeller($seller, $product);
+
+        $product->delete();
+
+        return $this->showOne($product);
+    }
+
+    protected function checkSeller(Seller $seller, Product $product){
+//        dd($seller->id.'-'.$product->seller->id);
+        if ($seller->id != $product->seller->id){
+            // HttpException is from SymphonyComponent
+            throw new HttpException(422,'The specified seller is not the actual seller of the product');
+        }
     }
 
 }
