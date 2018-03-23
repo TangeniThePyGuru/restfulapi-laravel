465472d37a33ce60398e7c80ab1f3e0062d9ff82
Tangeni <tangenishikomba@gmail.com>
2018-03-24 00:04:51 +0200

    Product Category Controller update method implemented

--- app/Http/Controllers/Product/ProductCategoryController.php
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Product;
 
+use App\Category;
 use App\Http\Controllers\ApiController;
 use App\Product;
+use Illuminate\Http\Request;
 
 class ProductCategoryController extends ApiController
 {
@@ -17,6 +19,22 @@ class ProductCategoryController extends ApiController
         $categories = $product->categories;
 
         return $this->showAll($categories);
+    }
+
+    /**
+     * @param Request $request
+     * @param Product $product
+     * @param Category $category
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, Product $product, Category $category){
+        // attach -> attaches the category and duplicates it if it existed already
+        // sync -> removes everything and lives this one
+        // syncWithoutDetach -> attaches the category without removing and duplicating
+        $product->categories()->syncWithoutDetaching([$category->id]);
+
+
+        return $this->showAll($product->categories);
     }
 
 }
