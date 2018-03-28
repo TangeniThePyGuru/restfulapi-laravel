23d2f2c7be147626a274ef9ca050393b04c3f0f0
Tangeni <tangenishikomba@gmail.com>
2018-03-28 11:48:35 +0200

    UserTransformer implemented

---
+++ app/Transformers/UserTransformer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Transformers;
+
+use App\User;
+use League\Fractal\TransformerAbstract;
+
+class UserTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     * @param User $user
+     * @return array
+     */
+    public function transform(User $user)
+    {
+        return [
+            'id' => (string) $user->id,
+            'name' => (string) $user->name,
+            'email' => (string) $user->email,
+            'isVerified' => (int) $user->verified,
+            'isAdmin' => ($user->admin === true),
+            'creationDate' => $user->created_at,
+            'lastChange' => $user->updated_at,
+            'deletedDate' => isset($user->deleted_at) ? (string) $user->deleted_at : null,
+
+        ];
+    }
+}
