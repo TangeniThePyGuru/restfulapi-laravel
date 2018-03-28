151e293f6e718a2c392a5804ade1ccfd8f525cef
Tangeni <tangenishikomba@gmail.com>
2018-03-28 13:30:04 +0200

    incomplete name error fixed

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -38,7 +38,7 @@ $factory->define(App\Product::class, function (Faker $faker) {
         'description' => $faker->paragraph(1),
         'quantity' => $faker->numberBetween(1,10),
         'status' => $faker->randomElement([\App\Product::AVAILABLE_PRODUCT, \App\Product::UNAVAILABLE_PRODUCT]),
-        'image' => $faker->randomElement(['1.jpeg', '2.jpg', 'jpg']),
+        'image' => $faker->randomElement(['1.jpeg', '2.jpg', '3.jpg']),
         'seller_id' => \App\User::all()->random()->id
 
     ];
