ecbc2ef612cf8e0f8b4cbbb3c0826274e1c3e97a
Tangeni <tangenishikomba@gmail.com>
2018-06-01 12:34:18 +0200

    scopes added to the user controller

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -17,8 +17,9 @@ class UserController extends ApiController
     {
         // protect the index and show controller
         $this->middleware('client.credentials')->only(['store', 'resend']);
-
+		$this->middleware('auth:api')->except(['store', 'verify', 'resend']);
         $this->middleware('transform.input:'. UserTransformer::class)->only(['store', 'update']);
+		$this->middleware('scope:manage-account')->only(['show', 'update']);
     }
 
     /**
