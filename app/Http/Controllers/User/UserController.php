46ce804df4970edc5bb6ad7b38017b91054ea8c4
Tangeni <tangenishikomba@gmail.com>
2018-06-03 01:42:20 +0200

    user policy added

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -15,11 +15,15 @@ class UserController extends ApiController
 
     public function __construct()
     {
-        // protect the index and show controller
+        // protect the index and show methods
         $this->middleware('client.credentials')->only(['store', 'resend']);
 		$this->middleware('auth:api')->except(['store', 'verify', 'resend']);
         $this->middleware('transform.input:'. UserTransformer::class)->only(['store', 'update']);
 		$this->middleware('scope:manage-account')->only(['show', 'update']);
+
+		$this->middleware('can:view,user')->only('show');
+		$this->middleware('can:update,user')->only('update');
+		$this->middleware('can:delete,user')->only('destroy');
     }
 
     /**
